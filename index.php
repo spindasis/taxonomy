9bd3d290eb60aab8b56b41c6cea65ad7c3df205c
spindasis <ictis>
2015-05-28 14:30:39 +0100

    start
    basic

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Home</title>
+</head>
+
+<body>
+ Test hello
+ <?php
+ 
+ echo $_REQUEST['taxonpage'],'<br>';
+  echo $_REQUEST['taxon_id'],'<br>';
+  echo $_REQUEST['taxon_name'],'<br>';
+  echo $_REQUEST['page'],'<br>';
+ ?>
+</body>
+</html>
